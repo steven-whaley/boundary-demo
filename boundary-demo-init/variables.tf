e554544b1396ecda0eda82d777c7d340938bf5fd
Omid Eghaneyan <56609570+omeeomi@users.noreply.github.com>
2023-04-12 19:17:58 -0700

    Update variables.tf

--- boundary-demo-init/variables.tf
+++ boundary-demo-init/variables.tf
@@ -1,7 +1,31 @@
+
+variable "hcp_client_id" {
+  description = "HCP Client ID to authenticate to HCP."
+  type = string
+}
+
+variable "hcp_client_secret" {
+  description = "HCP Client Secret to authenticate to HCP."
+  type = string
+  sensitive   = true
+}
+
+variable "tfc_org" {
+  description = "Terraform Cloud Organization name."
+  type = string
+}
+
+variable "workspace_name" {
+  description = "Terraform Cloud Workspace name."
+  type = string
+}
+
 variable "boundary_user" {
+  description = "Username for the Boundary User to be created."
   type = string
 }
 
 variable "boundary_password" {
+  description = "Password for the Boundary User to be created."
   type = string
 }
